52126a0ac39788e131366d011222a01eec04cc41
Sezgin Deniz Zeren <sezginzeren02@gmail.com>
2022-09-07 09:31:32 +0300

    bug fiz

--- blutv_clone/lib/repo/filmData.dart
+++ blutv_clone/lib/repo/filmData.dart
@@ -5,18 +5,18 @@ class Films {
   Future<List<Filmler>> filmleriGetir() async{
     var filmlerListesi= <Filmler>[];
 
-    var f1 = Filmler(filmId: 1,  filmResimAdi: "jonh_wick.png", );
-    var f2 = Filmler(filmId: 2,  filmResimAdi: "jonh_wick.png" );
-    var f3 = Filmler(filmId: 3,  filmResimAdi: "jonh_wick.png" );
-    var f4 = Filmler(filmId: 4,  filmResimAdi: "jonh_wick.png", );
-    var f5 = Filmler(filmId: 5,  filmResimAdi: "jonh_wick.png", );
-    var f6 = Filmler(filmId: 6,  filmResimAdi: "jonh_wick.png" );
-    var f7 = Filmler(filmId: 1,  filmResimAdi: "jonh_wick.png");
-    var f8 = Filmler(filmId: 2,  filmResimAdi: "jonh_wick.png");
-    var f9 = Filmler(filmId: 3,  filmResimAdi: "jonh_wick.png" );
-    var f10 = Filmler(filmId: 4,  filmResimAdi: "jonh_wick.png", );
-    var f11= Filmler(filmId: 5,  filmResimAdi: "jonh_wick.png" );
-    var f12 = Filmler(filmId: 6,  filmResimAdi:"jonh_wick.png" );
+    var f1 = Filmler(filmId: 1,  filmResimAdi: "john-wick.png", );
+    var f2 = Filmler(filmId: 2,  filmResimAdi: "fight-club.jpg" );
+    var f3 = Filmler(filmId: 3,  filmResimAdi: "godfather.jpg" );
+    var f4 = Filmler(filmId: 4,  filmResimAdi: "revenant.jpg", );
+    var f5 = Filmler(filmId: 5,  filmResimAdi: "lor.jpg", );
+    var f6 = Filmler(filmId: 6,  filmResimAdi: "imlegend.jpg" );
+    var f7 = Filmler(filmId: 7,  filmResimAdi: "deadpool.jpg");
+    var f8 = Filmler(filmId: 8,  filmResimAdi: "thewolfofwallstreet.jpg");
+    var f9 = Filmler(filmId: 9,  filmResimAdi: "grindelwald.jpg" );
+    var f10 = Filmler(filmId: 10,  filmResimAdi:"300.jpg", );
+    var f11= Filmler(filmId: 11,  filmResimAdi: "batman.jpg" );
+    var f12 = Filmler(filmId: 12,  filmResimAdi:"fast.jpg" );
 
     filmlerListesi.add(f1);
     filmlerListesi.add(f2);
--- blutv_clone/lib/views/home_page.dart
+++ blutv_clone/lib/views/home_page.dart
@@ -17,37 +17,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
- Future<List<Filmler>> filmleriGetir() async{
-    var filmlerListesi= <Filmler>[];
 
-    var f1 = Filmler(filmId: 1,  filmResimAdi: "john-wick.png", );
-    var f2 = Filmler(filmId: 2,  filmResimAdi: "fight-club.jpg" );
-    var f3 = Filmler(filmId: 3,  filmResimAdi: "godfather.jpg" );
-    var f4 = Filmler(filmId: 4,  filmResimAdi: "revenant.jpg", );
-    var f5 = Filmler(filmId: 5,  filmResimAdi: "lor.jpg", );
-    var f6 = Filmler(filmId: 6,  filmResimAdi: "imlegend.jpg" );
-    var f7 = Filmler(filmId: 7,  filmResimAdi: "deadpool.jpg");
-    var f8 = Filmler(filmId: 8,  filmResimAdi: "thewolfofwallstreet.jpg");
-    var f9 = Filmler(filmId: 9,  filmResimAdi: "grindelwald.jpg" );
-    var f10 = Filmler(filmId: 10,  filmResimAdi:"300.jpg", );
-    var f11= Filmler(filmId: 11,  filmResimAdi: "batman.jpg" );
-    var f12 = Filmler(filmId: 12,  filmResimAdi:"fast.jpg" );
-
-    filmlerListesi.add(f1);
-    filmlerListesi.add(f2);
-    filmlerListesi.add(f3);
-    filmlerListesi.add(f4);
-    filmlerListesi.add(f5);
-    filmlerListesi.add(f6);
-    filmlerListesi.add(f7);
-    filmlerListesi.add(f8);
-    filmlerListesi.add(f9);
-    filmlerListesi.add(f10);
-    filmlerListesi.add(f11);
-    filmlerListesi.add(f12);
-
-    return filmlerListesi;
-  }
+  var f=Films();
   int currentIndex=0;
 
   void onTap(int index){
@@ -112,7 +83,7 @@ class _HomePageState extends State<HomePage> {
         
         body:        
          FutureBuilder<List<Filmler>>(
-          future: filmleriGetir(),
+          future: f.filmleriGetir(),
           
           builder: (context, snapshot) {
             if (snapshot.hasData) {
@@ -197,42 +168,6 @@ class _HomePageState extends State<HomePage> {
       
     );
 
-     /* DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        
-        appBar: AppBar(
-          backgroundColor: bgColor,
-          bottom: const TabBar(
-
-            tabs: [
-            Tab(icon: Icon(BlutvIcon.blutv_icon),height: 20,iconMargin:EdgeInsets.only(right:50.0) ,),
-            Tab(icon: Text("Filmler", style: TextStyle(fontFamily: "Regular", fontSize: 18),),),
-            Tab(icon: Text("Diziler", style: TextStyle(fontFamily: "Regular", fontSize: 18),),),
-            Tab(icon: Icon(BlutvIcon.kids),)
-          ],
-          ),
-         
-        
-      ),
-      
-      body: TabBarView(
-        children: [
-          
-          Card(child: Column( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          
-                      
-                      children: [
-                
-                        Image.asset("resimler/${filmlerListesi.}"),
-                       
-                      ],
-                      ),
-              )
-        ],
-      )
-      
-      ),
-    ); */
+   
   }
 }
